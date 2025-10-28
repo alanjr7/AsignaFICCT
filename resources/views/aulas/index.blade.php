8b2afc5f66fc03a733d879ca5e4cc31dbda7a424
alan <Drake@DESKTOP-2C494C7>
2025-10-28 07:13:44 -0400

    mm

--- resources/views/aulas/index.blade.php
+++ resources/views/aulas/index.blade.php
@@ -6,7 +6,7 @@
 <div class="flex justify-between items-center mb-6">
     <h2 class="text-2xl font-bold text-gray-800">Gestión de Aulas</h2>
     <a href="{{ route('aulas.create') }}" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg transition duration-200">
-        + Crear Aula
+        + Añadir Aula
     </a>
 </div>
 
