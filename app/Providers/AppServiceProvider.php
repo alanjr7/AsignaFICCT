3f8ec08a0f193c2feaf0276e689e103064f1ed8c
almaRibera <almapropia44@gmail.com>
2025-11-08 20:01:38 -0400

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
          if ($this->app->environment('production')) {
-        URL::forceScheme('http');
+        URL::forceScheme('https');
           }
-           URL::forceScheme('http');
+           URL::forceScheme('https');
     }
 }
