964909a9cb95ff566abe29f20fbe4298cd009474
almaRibera <almapropia44@gmail.com>
2025-11-12 20:21:16 -0400

    ...

--- database/migrations/2025_10_24_183517_create_horarios_table.php
+++ database/migrations/2025_10_24_183517_create_horarios_table.php
@@ -1,32 +1,32 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
+    return new class extends Migration
     {
-        Schema::create('horarios', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('grupo_materia_id')->constrained('grupo_materia')->onDelete('cascade');
-            $table->foreignId('aula_id')->constrained()->onDelete('cascade');
-            $table->enum('dia', ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']);
-            $table->time('hora_inicio');
-            $table->time('hora_fin');
-            $table->timestamps();
+        public function up(): void
+        {
+            Schema::create('horarios', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('grupo_materia_id')->constrained('grupo_materia')->onDelete('cascade');
+                $table->foreignId('aula_id')->constrained()->onDelete('cascade');
+                $table->enum('dia', ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']);
+                $table->time('hora_inicio');
+                $table->time('hora_fin');
+                $table->timestamps();
 
-            // Evitar conflictos de aula en mismo horario
-            $table->unique(['aula_id', 'dia', 'hora_inicio']);
-            
-            // Evitar que mismo docente tenga dos clases al mismo tiempo
-            $table->unique(['grupo_materia_id', 'dia', 'hora_inicio']);
-        });
-    }
+                // Evitar conflictos de aula en mismo horario
+                $table->unique(['aula_id', 'dia', 'hora_inicio']);
+                
+                // Evitar que mismo docente tenga dos clases al mismo tiempo
+                $table->unique(['grupo_materia_id', 'dia', 'hora_inicio']);
+            });
+        }
 
-    public function down(): void
-    {
-        Schema::dropIfExists('horarios');
-    }
-};
+        public function down(): void
+        {
+            Schema::dropIfExists('horarios');
+        }
+    };
